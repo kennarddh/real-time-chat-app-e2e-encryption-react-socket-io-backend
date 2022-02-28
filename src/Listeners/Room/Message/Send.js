05c480ba9af9d251b6bd24bfa98eec11ae8ffb08
kennarddh <kennarddh@gmail.com>
2022-02-28 19:46:58 +0700

    id for every messages

--- src/Listeners/Room/Message/Send.js
+++ src/Listeners/Room/Message/Send.js
@@ -1,11 +1,12 @@
 import { GetUserById } from '../../../Services/Users'
 
-const Send = (socket, { message }) => {
+const Send = (socket, { message, id }) => {
 	const user = GetUserById(socket.id)
 
 	socket.to(user.room).emit('room:message:receive', {
 		from: user.username,
 		message,
+		id,
 	})
 }
 
